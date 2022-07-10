490d88d761c2ccbc7c47d4c36571ec9c5f1857a2
Justin Dannemiler <jdannem6@kent.edu>
2022-07-10 17:38:12 -0400

    Added functionality to drop undesired attributes

--- test.py
+++ test.py
@@ -13,19 +13,25 @@ records = [] # list of records in dataset
 # # print the first 20 records
 # row1 = dataframe.sample(1)
 # print(row1)
-
+# Read in the Dataset
 accidentDataframe = dataframe.read_csv(datasetFileName)
+# Store all attriute names
+attributeNames = accidentDataframe.columns
 
+## Drop all columns that are not of interest to research question
+researchAttributes = [] # attributes of interest
+# Directly insert attributes of interest into this list
+""" Later we can add code to prompt the user for these attribute names """
+researchAttributes.append('Start_Time')
+researchAttributes.append('State')
+researchAttributes.append('County')
 
-# # Read the original dataset from csv file
-# with open(datasetFileName, "r") as datasetFile:
-#     # create csv reader object
-#     accidentDataset = csv.reader(datasetFile)
+# Get list of all attributes that are not of interst
+throwawayAttributes = []
+for attribute in attributeNames:
+    # Insert attribute in throwaway attribute if it is not a researchAttribute
+    if attribute not in researchAttributes:
+        throwawayAttributes.append(attribute)
+# Drop all attributes that are included in throwawayAttributes
+accidentDataframe.drop(columns=throwawayAttributes)
 
-#     # Extact the attribute names from file
-#     attributes = next(accidentDataset)
-
-#     # Extract first ten records
-#     recordIndex = 0
-#     for record in accidentDataset[:10]:
-#         records.append(record)
